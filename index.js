e9e6bdec7e8cd747c252a719766d053d545dae5a
rusty619 <rusty6916@gmail.com>
2022-03-15 12:01:40 -0400

    commit tree c0d402d441a5e9aa1d99b9bb3b9c32c901aded13

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
+  let str = ""
   // Write your code here!
+  if (num <= 400) str = "This one is on me!";
+  else if (num > 2000 && num < 2500) str = 'I will gladly take your thirty bucks.';
+  else if (num > 2500) str = 'No can do.';
+  return str;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(str){
   // Write your code here!
+  if (str === 'NYC') return ('Ok, sounds good.')
+  else if (str === 'Pittsburgh') return ('No go.')
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(str){
   // Write your code here!
+  if (str === 'generous') return ('Thank you so much.')
+  else if (str === 'not as generous') return ('Thank you.')
+  else if (str === 'thanks for everything') return('Bye.')
 }
+
+var x = scuberGreetingForFeet(2501)
+console.log(x)
